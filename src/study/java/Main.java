d6d234576871c31ba02b3dca426e57a2ebb6b66d
unknown <eva@ehrstedt.net>
2020-02-09 22:24:51 +0100

    first commit

---
+++ src/study/java/Main.java
@@ -0,0 +1,14 @@
+package study.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main main = new Main();
+		main.hello();
+	}
+
+	public void hello() {
+		System.out.println("Hello everyone!");
+	}
+}
---
+++ src/study/java/Tests.java
@@ -0,0 +1,11 @@
+package study.java;
+
+import org.junit.Test;
+
+public class Tests {
+
+	@Test
+	public void testHelloMethod() {
+		// fail("Not yet implemented");
+	}
+}
